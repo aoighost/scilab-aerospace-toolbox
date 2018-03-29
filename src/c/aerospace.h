2478a744f9a307377fcd3b97557a548556c84751
aoighost <th3flyboy@gmail.com>
2018-03-29 03:21:56 -0400

    Ever look back at your old code and realize "wait, how the fuck did I not change the license from the skeleton files?
    Fail.

--- src/c/aerospace.h
+++ src/c/aerospace.h
@@ -2,6 +2,10 @@
  *  Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
  *  Copyright (C) 2011-2011 - DIGITEO - Clément DAVID
  *
+ * 
+ *  This file was written as a PoC for Google Summer of Code
+ *  Copyright (C) 2011-2012 - Peter Clemenko III
+ *
  *  This file must be used under the terms of the CeCILL.
  *  This source file is licensed as described in the file COPYING, which
  *  you should have received as part of this distribution.  The terms
